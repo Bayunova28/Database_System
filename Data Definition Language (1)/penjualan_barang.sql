5fa1e4fbbe7cae8f5f98bb6c7b4cbbf429e51611
Willibrordus Bayu <willibayu28@gmail.com>
2022-05-29 21:03:51 +0700

    First Commit

--- Data Definition Language (1)/penjualan_barang.sql
+++ Data Definition Language (1)/penjualan_barang.sql
@@ -6,123 +6,108 @@
 -- Generation Time: Dec 30, 2021 at 06:33 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET 
+  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+SET 
+  time_zone = "+00:00";
 --
 -- Database: `penjualan_barang`
 --
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `barang`
 --
-
 CREATE TABLE `barang` (
-  `Id_Barang` char(5) NOT NULL,
-  `Nama_Barang` varchar(20) NOT NULL,
-  `Harga` int(11) NOT NULL,
+  `Id_Barang` char(5) NOT NULL, 
+  `Nama_Barang` varchar(20) NOT NULL, 
+  `Harga` int(11) NOT NULL, 
   `Stok` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `detail_jual`
 --
-
 CREATE TABLE `detail_jual` (
-  `Id_Nota` char(5) NOT NULL,
-  `Id_Barang` char(5) NOT NULL,
-  `Jumlah` int(11) NOT NULL,
+  `Id_Nota` char(5) NOT NULL, 
+  `Id_Barang` char(5) NOT NULL, 
+  `Jumlah` int(11) NOT NULL, 
   `Harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `nota_penjualan`
 --
-
 CREATE TABLE `nota_penjualan` (
-  `Id_Nota` char(5) NOT NULL,
-  `Tgl_Nota` date NOT NULL,
-  `Total` int(11) NOT NULL,
+  `Id_Nota` char(5) NOT NULL, 
+  `Tgl_Nota` date NOT NULL, 
+  `Total` int(11) NOT NULL, 
   `Id_Pelanggan` char(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `pelanggan`
 --
-
 CREATE TABLE `pelanggan` (
-  `Id_Pelanggan` char(5) NOT NULL,
-  `Nama` varchar(20) NOT NULL,
-  `Alamat` varchar(30) NOT NULL,
-  `Pekerjaan` varchar(20) NOT NULL,
-  `No_Ktp` int(11) NOT NULL,
+  `Id_Pelanggan` char(5) NOT NULL, 
+  `Nama` varchar(20) NOT NULL, 
+  `Alamat` varchar(30) NOT NULL, 
+  `Pekerjaan` varchar(20) NOT NULL, 
+  `No_Ktp` int(11) NOT NULL, 
   `No_telp` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 --
 -- Indexes for dumped tables
 --
-
 --
 -- Indexes for table `barang`
 --
-ALTER TABLE `barang`
-  ADD PRIMARY KEY (`Id_Barang`);
-
+ALTER TABLE 
+  `barang` 
+ADD 
+  PRIMARY KEY (`Id_Barang`);
 --
 -- Indexes for table `detail_jual`
 --
-ALTER TABLE `detail_jual`
-  ADD PRIMARY KEY (`Id_Nota`,`Id_Barang`),
-  ADD KEY `Id_Barang` (`Id_Barang`);
-
+ALTER TABLE 
+  `detail_jual` 
+ADD 
+  PRIMARY KEY (`Id_Nota`, `Id_Barang`), 
+ADD 
+  KEY `Id_Barang` (`Id_Barang`);
 --
 -- Indexes for table `nota_penjualan`
 --
-ALTER TABLE `nota_penjualan`
-  ADD PRIMARY KEY (`Id_Nota`),
-  ADD KEY `Id_Pelanggan` (`Id_Pelanggan`);
-
+ALTER TABLE 
+  `nota_penjualan` 
+ADD 
+  PRIMARY KEY (`Id_Nota`), 
+ADD 
+  KEY `Id_Pelanggan` (`Id_Pelanggan`);
 --
 -- Indexes for table `pelanggan`
 --
-ALTER TABLE `pelanggan`
-  ADD PRIMARY KEY (`Id_Pelanggan`);
-
+ALTER TABLE 
+  `pelanggan` 
+ADD 
+  PRIMARY KEY (`Id_Pelanggan`);
 --
 -- Constraints for dumped tables
 --
-
 --
 -- Constraints for table `detail_jual`
 --
-ALTER TABLE `detail_jual`
-  ADD CONSTRAINT `detail_jual_ibfk_1` FOREIGN KEY (`Id_Nota`) REFERENCES `nota_penjualan` (`Id_Nota`),
-  ADD CONSTRAINT `detail_jual_ibfk_2` FOREIGN KEY (`Id_Barang`) REFERENCES `barang` (`Id_Barang`);
-
+ALTER TABLE 
+  `detail_jual` 
+ADD 
+  CONSTRAINT `detail_jual_ibfk_1` FOREIGN KEY (`Id_Nota`) REFERENCES `nota_penjualan` (`Id_Nota`), 
+ADD 
+  CONSTRAINT `detail_jual_ibfk_2` FOREIGN KEY (`Id_Barang`) REFERENCES `barang` (`Id_Barang`);
 --
 -- Constraints for table `nota_penjualan`
 --
-ALTER TABLE `nota_penjualan`
-  ADD CONSTRAINT `nota_penjualan_ibfk_1` FOREIGN KEY (`Id_Pelanggan`) REFERENCES `pelanggan` (`Id_Pelanggan`);
+ALTER TABLE 
+  `nota_penjualan` 
+ADD 
+  CONSTRAINT `nota_penjualan_ibfk_1` FOREIGN KEY (`Id_Pelanggan`) REFERENCES `pelanggan` (`Id_Pelanggan`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
